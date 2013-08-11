f866d9d635abd695b5ca1c5e0a3e8d1def6884b1
Dan Draper <daniel@codefire.com>
2013-08-12 09:17:09 +1000

    Cleaned up environment code

--- lib/ey-provisioner/environment.rb
+++ lib/ey-provisioner/environment.rb
@@ -6,20 +6,12 @@ module Ey
         @env_id     = env_id
       end
 
-      def add_instance(options = {})
-        api_error_handler do
-          request  = Request.new(options)
-          response = @connection.http_post(path_for(:add_instances), request)
-          Instance.new(JSON(response.body)['instance'])
-        end
+      def remove_instances(options = {})
+        process_post(path_for(:remove_instances), options)
       end
 
-      def remove_instances(options = {})
-        api_error_handler do
-          request  = Request.new(options)
-          response = @connection.http_post(path_for(:remove_instances), request)
-          Instance.new(JSON(response.body)['instance'])
-        end
+      def add_instance(options = {})
+        process_post(path_for(:add_instances), options)
       end
 
       # TODO: Handle add and remove status
@@ -44,6 +36,14 @@ module Ey
             yield if block_given?
           rescue Excon::Errors::Error => e
             raise APIError.new(e)
+          end
+        end
+
+        def process_post(path, options)
+          api_error_handler do
+            request  = Request.new(options)
+            response = @connection.http_post(path, request)
+            Instance.new(JSON(response.body)['instance'])
           end
         end
     end
